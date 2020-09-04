89edfec30496efcf98ae9c87cc574ba2968d011f
Fredrik Ås <fredaas86@gmail.com>
2020-09-04 12:07:22 +0200

    Use ROOT_DIR everywhere
    Also clean up extracted files.

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -17,25 +17,30 @@ mkdir -p local
 # Install libevent
 cd libevent-2.1.11-stable
 ./autogen.sh
-./configure --prefix=${HOME}/local
+./configure --prefix=${ROOT_DIR}/local
 make
 make install
 cd ..
 
 # Install ncurses
 cd ncurses-6.2
-./configure --prefix=${HOME}/local
+./configure --prefix=${ROOT_DIR}/local
 make
 make install
 cd ..
 
 # Install tmux
 cd tmux-3.0a
-./configure --prefix=${HOME}/local \
-    CFLAGS="-I${HOME}/local/include -I${HOME}/local/include/ncurses" \
-    LDFLAGS="-L${HOME}/local/include -L${HOME}/local/include/ncurses -L${HOME}/local/lib"
+./configure --prefix=${ROOT_DIR}/local \
+    CFLAGS="-I${ROOT_DIR}/local/include -I${ROOT_DIR}/local/include/ncurses" \
+    LDFLAGS="-L${ROOT_DIR}/local/include -L${ROOT_DIR}/local/include/ncurses -L${ROOT_DIR}/local/lib"
 make
 make install
 cd ..
 
-echo "[SUCCESS] ${HOME}/local/bin/tmux is now available"
+# Clean up
+rm -rf libevent-2.1.11-stable*
+rm -rf ncurses-6.2*
+rm -rf tmux-3.0a*
+
+echo "[SUCCESS] ${ROOT_DIR}/local/bin/tmux is now available"
